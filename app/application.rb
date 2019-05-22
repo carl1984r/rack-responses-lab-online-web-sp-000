ed25c23e52897edf2b9ac4d8a31ccf43fd3aae89
Bry Cox <carl.cc@gmail.com>
2019-05-22 19:28:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,10 @@
-require 'pry'
 class Application
 
   def call(env)
     resp = Rack::Response.new
 
     time = Time.new
-      binding.pry
+    
     if time.hour < 12
       resp.write "Morning"
     else
