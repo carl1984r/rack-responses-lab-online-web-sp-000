65dfe133573d021a07b3e7c259588f098a1e9e6a
Bry Cox <carl.cc@gmail.com>
2019-05-22 19:16:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,12 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
+    
+    if num_1==num_2 && num_2==num_3
+      resp.write "Morning"
+    else
+      resp.write "Afternoon"
+    end
 
 
     resp.finish
